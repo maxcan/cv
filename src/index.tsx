d76f2b54adf951a78a67cbc12252d315842ec9e5
Max <max.cantor@gonitro.com>
2016-10-31 15:50:02 -0700

    small bumps

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import * as moment from "moment"
 import SkillsTable from "./skills"
 require("./styles.css")
 
-const cv:Cv = require("!json!yaml!../cv.yaml") as Cv
+const cv: Cv = require("!json!yaml!../cv.yaml") as Cv
 
 declare var require: {
     <T>(path: string): T
@@ -13,7 +13,7 @@ declare var require: {
     // ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
 }
 
-const CvSocial = ({social}: {social: Social}) => {
+const CvSocial = ({social}: { social: Social }) => {
     return (
         <ul>
             {social.linkedIn && <li>{social.linkedIn}</li>}
@@ -21,56 +21,56 @@ const CvSocial = ({social}: {social: Social}) => {
         </ul>
     )
 }
-const CvWorkSkills = ({skills}: {skills:WorkSkills}) => {
+const CvWorkSkills = ({skills}: { skills: WorkSkills }) => {
     return (
         <ul className="skills">
-            { skills.full && _.map(skills.full, (s, idx) => { <li key={idx}><strong>{s}</strong></li> })}
-            { skills.partial && _.map(skills.partial, (s, idx) => { <li key={999+idx}>{s}</li> })}
+            {skills.full && _.map(skills.full, (s, idx) => { <li key={idx}><strong>{s}</strong></li> })}
+            {skills.partial && _.map(skills.partial, (s, idx) => { <li key={999 + idx}>{s}</li> })}
         </ul>
     )
 }
 
-const CvWork = ({allWork}: {allWork: Array<WorkExperience>}) => {
+const CvWork = ({allWork}: { allWork: Array<WorkExperience> }) => {
     return (
         <article id="work" className="work">
             <h1>Work Experience</h1>
-            {_.map(allWork, (work, idx) => 
+            {_.map(allWork, (work, idx) =>
                 <section key={idx}>
                     <h2>{work.position}</h2>
                     <h3>
                         {work.company},&nbsp;<em>{work.loc}</em>
                     </h3>
-                    <p><DtRng st={work.start} en={work.end}/></p>
+                    <p><DtRng st={work.start} en={work.end} /></p>
                     <ul className="highlights">
-                        {_.map(work.highlights, (h, idx) => <li key={idx}>{h}</li> ) }
+                        {_.map(work.highlights, (h, idx) => <li key={idx}>{h}</li>)}
                     </ul>
-                    { work.skills && <CvWorkSkills skills={work.skills}/>}
+                    {work.skills && <CvWorkSkills skills={work.skills} />}
                 </section>
             )}
         </article>
     )
 }
 
-const CvEducation = ({allEdu}: {allEdu: Education[]}) => {
+const CvEducation = ({allEdu}: { allEdu: Education[] }) => {
     return (
         <article id="education" className="education">
             <h1>Education</h1>
-            {_.map(allEdu, (edu, idx) => 
+            {_.map(allEdu, (edu, idx) =>
                 <section key={idx}>
                     <h2>{edu.degree}</h2>
                     <h3>{edu.institution}</h3>
-                    <p><DtRng st={edu.start} en={edu.end}/></p>
+                    <p><DtRng st={edu.start} en={edu.end} /></p>
                 </section>
             )}
         </article>
     )
 }
 
-const CvPublications = ({allPub}: {allPub: Publication[]}) => {
+const CvPublications = ({allPub}: { allPub: Publication[] }) => {
     return (
         <article id="publications" className="publication">
             <h1>Publications</h1>
-            {_.map(allPub, (pub, idx) => 
+            {_.map(allPub, (pub, idx) =>
                 <section key={idx}>
                     <a href={pub.url}><h2>{pub.name}</h2></a>
                     <a href={pub.url}><p>{pub.description}</p></a>
@@ -80,57 +80,59 @@ const CvPublications = ({allPub}: {allPub: Publication[]}) => {
     )
 }
 
-const CvCertifications = ({allCerts}: {allCerts: Certification[]}) => {
+const CvCertifications = ({allCerts}: { allCerts: Certification[] }) => {
     return (
         <article id="certifications" className="certification">
             <h1>Certifications</h1>
-            {_.map(allCerts, (cert, idx) => 
+            {_.map(allCerts, (cert, idx) =>
                 <section key={idx}>
                     <h2>{cert.name}</h2>
-                    <p><Dt dt={cert.start}/></p>
+                    <p><Dt dt={cert.start} /></p>
                 </section>
             )}
         </article>
     )
 }
 
-const DtRng = ({st, en}: {st: Date, en?: Date}) => {
-    return <span className="dateRange"><Dt dt={st}/>&nbsp;-&nbsp;{en ? <Dt dt={en}/> : <span>Present</span>}</span>
+const DtRng = ({st, en}: { st: Date, en?: Date }) => {
+    return <span className="dateRange"><Dt dt={st} />&nbsp;-&nbsp;{en ? <Dt dt={en} /> : <span>Present</span>}</span>
 }
-const Dt = ({dt}: {dt: Date}) => {
+const Dt = ({dt}: { dt: Date }) => {
     return <span className="date">{moment(dt).format("MMMM YYYY")}</span>
 }
-const CvBasic = ({basic}: {basic: Basic}) => {
+const CvBasic = ({basic}: { basic: Basic }) => {
     return (
-        <header> 
+        <div>
             <div className="clearfix">
                 <h1>{basic.name}</h1>
-                { basic.title && <h2>{basic.title}</h2> }
+                {basic.title && <h2>{basic.title}</h2>}
                 <div className="pull-right">
                     <h3>{basic.loc}</h3>
-                    { basic.phone && <h3>{basic.phone}</h3> }
-                { basic.email && <h3>{basic.email}</h3> }
+                    {basic.phone && <h3>{basic.phone}</h3>}
+                    {basic.email && <h3>{basic.email}</h3>}
                 </div>
             </div>
             <hr />
-            { basic.passions &&
+            {basic.passions &&
                 <span>
                     passions in life:&nbsp;
-                    <strong>
-                        {basic.passions.map((s:string) => <span className="comma-sep" key={s}>{s}</span> )}
-                    </strong>
+                    <ul className="strong comma-sep">
+                        {basic.passions.map((s: string) =>
+                           <li className="comma-sep" key={s}><strong>{s}</strong></li>
+                        )}
+                    </ul>
                 </span>
             }
-            { basic.social && <CvSocial social={basic.social}/>}
-        </header>
+            {basic.social && <CvSocial social={basic.social} />}
+        </div>
     )
 }
 // class CvRoot extends React.Component<{cv: Cv}, {} > {
-const CvRoot = ({cv}: {cv: Cv}) => {
+const CvRoot = ({cv}: { cv: Cv }) => {
     return (
-        <div className="cv-main container" data-spy="scroll" data-target="#navbar">
+        <div className="cv-main " data-spy="scroll" data-target="#navbar">
             <nav id="navbar" className="navbar navbar-default navbar-fixed-top">
-                <div className="container">
+                <div className="container-fluid">
                     <div className="navbar-header">
                         <a className="navbar-brand" href="#">CV Navigation</a>
                     </div>
@@ -145,18 +147,26 @@ const CvRoot = ({cv}: {cv: Cv}) => {
                     </ul>
                 </div>
             </nav>
-            <div className="cv-body">
-                <CvBasic basic={cv.basic} />
-                <hr/>
-                <CvWork allWork={cv.workExperience} />
-                <hr/>
-                <CvEducation allEdu={cv.education} />
-                <hr/>
-                <CvCertifications allCerts={cv.certifications} />
-                <hr/>
-                <CvPublications allPub={cv.publications} />
-                <hr/>
-                <div id="skills_table"><SkillsTable cv={cv}/></div>
+            <div className="container-fluid cv-body">
+                <div className="row">
+                <div className="col-md-3 ">
+                    <div className="afxfix">
+                        <CvBasic basic={cv.basic} />
+                    </div>
+                </div>
+                <div className="pull-right col-md-9">
+                    <hr />
+                    <CvWork allWork={cv.workExperience} />
+                    <hr />
+                    <CvEducation allEdu={cv.education} />
+                    <hr />
+                    <CvCertifications allCerts={cv.certifications} />
+                    <hr />
+                    <CvPublications allPub={cv.publications} />
+                    <hr />
+                    <div id="skills_table"><SkillsTable cv={cv} /></div>
+                </div>
+            </div>
             </div>
         </div>
     )
@@ -166,7 +176,7 @@ const CvRoot = ({cv}: {cv: Cv}) => {
 // $.get(cvYamlUrl)
 //     .done(rawYaml => {
 //         const cv:Cv = YAML.parse(rawYaml) as Cv
-        ReactDOM.render(
-            <CvRoot cv={cv} />
-            , document.getElementById("root"))
+ReactDOM.render(
+    <CvRoot cv={cv} />
+    , document.getElementById("root"))
     // })
